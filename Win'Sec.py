0634039b6897e3d16925a1acbcbc2ec44a0e2046
steven-itech <149870787+steven-itech@users.noreply.github.com>
2024-10-13 22:08:13 +0200

    Update Win'Sec.py

--- Win'Sec.py
+++ Win'Sec.py
@@ -23,6 +23,11 @@ def clean():
 
     os.system("cls")
 
+
+def pause():
+
+    os.system("pause > nul")
+
 def path():
 
     return os.path.dirname(os.path.abspath(__file__))
@@ -126,40 +131,6 @@ def volatility():
                     tts(volatility_failed)
                     messagebox.showwarning(title="Win'Sec :", message=volatility_failed)
 
-def config():
-   
-    volatility_path = os.path.join(path(), "vol.py")
-    config_json = os.path.join(path(), "config.json")
-    memory_path = os.path.join(path(), "memory.mem")
-
-    paths = [volatility_path, config_json, memory_path]
-
-    if all(os.path.exists(p) for p in paths):
-            
-            config_command = subprocess.Popen(["python.exe", volatility_path, "--save-config", config_json, "-f", memory_path, "windows.info"], stdout=subprocess.PIPE)
-            config_command.communicate()
-
-            if os.path.exists(config_json):
-               
-                json_success = "Le fichier de configuration JSON vient d'être créé !"
-                
-                tts(json_success)
-                messagebox.showinfo(title="Win'Sec :", message=json_success)
-           
-            else:
-                
-                json_failed = "Le fichier de configuration JSON n'a pas pu être créé !"
-                
-                tts(json_failed)
-                messagebox.showwarning(title="Win'Sec :", message=json_failed)
-
-    else:
-
-       warning = "Veuillez télécharger l'ensemble des logiciels et créer une image mémoire !"
-
-       tts(warning)
-       messagebox.showwarning(title="Win'Sec :", message=warning)
-
 def download():
     
     try:
@@ -179,6 +150,45 @@ def download():
         tts(no_connection)
         messagebox.showwarning(title="Win'Sec :", message=no_connection)
 
+def delete():
+    
+    winpmem_path = os.path.join(path(), "winpmem.exe")
+    volatility_path = os.path.join(path(), "vol.py")
+    
+    if os.path.exists(winpmem_path):
+        
+        try:
+            
+            os.remove(winpmem_path)
+            winpmem_success = "WinPmem vient d'être supprimé !"
+            
+            tts(winpmem_success)
+            messagebox.showinfo(title="Win'Sec :", message=winpmem_success)
+        
+        except Exception:
+            
+            winpmem_failed = "WinPmem n'a pas pu être supprimé !"
+            
+            tts(winpmem_failed)
+            messagebox.showerror(title="Win'Sec :", message=winpmem_failed)
+
+    if os.path.exists(volatility_path):
+        
+        try:
+            
+            os.remove(volatility_path)
+            volatility_success = "Volatility vient d'être supprimé !"
+            
+            tts(volatility_success)
+            messagebox.showinfo(title="Win'Sec :", message=volatility_success)
+        
+        except Exception:
+            
+            volatility_failed = "Volatility n'a pas pu être supprimé !"
+            
+            tts(volatility_failed)
+            messagebox.showerror(title="Win'Sec :", message=volatility_failed)
+            
 def memory():
 
     winpmem_path = os.path.join(path(), "winpmem.exe")
@@ -213,6 +223,41 @@ def memory():
         messagebox.showwarning(title="Win'Sec :", message=warning_1)
         messagebox.showwarning(title="Win'Sec :", message=warning_2)
 
+def config():
+   
+    volatility_path = os.path.join(path(), "vol.py")
+    memory_path = os.path.join(path(), "memory.mem")
+
+    paths = [volatility_path, memory_path]
+
+    if all(os.path.exists(p) for p in paths):
+            
+            config_command = subprocess.Popen(["python.exe", volatility_path, "--save-config", "config.json", "-f", memory_path, "windows.info"], stdout=subprocess.PIPE)
+            config_command.communicate()
+
+            config_json = os.path.join(path(), "config.json")
+
+            if os.path.exists(config_json):
+               
+                json_success = "Le fichier de configuration JSON vient d'être créé !"
+                
+                tts(json_success)
+                messagebox.showinfo(title="Win'Sec :", message=json_success)
+           
+            else:
+                
+                json_failed = "Le fichier de configuration JSON n'a pas pu être créé !"
+                
+                tts(json_failed)
+                messagebox.showwarning(title="Win'Sec :", message=json_failed)
+
+    else:
+
+       warning = "Veuillez télécharger l'ensemble des logiciels et créer une image mémoire !"
+
+       tts(warning)
+       messagebox.showwarning(title="Win'Sec :", message=warning)
+
 def warning():
 
     softwares_warning = "Veuillez télécharger l'ensemble des logiciels, créer un fichier de configuration et une image mémoire !"
@@ -230,9 +275,14 @@ def pslist():
 
     if all(os.path.exists(p) for p in paths):
         
-        pslist = subprocess.Popen(["python.exe", volatility_path, "-c", config_json, "-f", memory_path, "windows.pslist.PsList"], stdout=subprocess.PIPE)
-        output, _ = pslist.communicate()
+        pslist_process = subprocess.Popen(["python.exe", volatility_path, "-c", config_json, "-f", memory_path, "windows.pslist.PsList"], stdout=subprocess.PIPE)
+        output, _ = pslist_process.communicate()
         output = output.decode("utf-8")
+
+        print(output)
+        
+        pause()
+        clean()
 
         pslist_file = "Souhaitez-vous enregistrer le résultat de l'analyse des processus exécutés ?"
         tts(pslist_file)
@@ -240,26 +290,30 @@ def pslist():
         pslit_window = tk.Tk()
         pslit_window.title("Enregistrer le résultat de l'analyse des processus exécutés :")
         pslit_window.iconbitmap(os.path.join(path(), "icon.ico"))
-        pslit_window.geometry("600x150")
+        pslit_window.geometry("600x120")
         pslit_window.resizable(False, False)
 
-        label = tk.Label(pslit_window, text=f"{pslist_file} :", padx=10, pady=10)
+        label = tk.Label(pslit_window, text=pslist_file, padx=10, pady=10)
         label.pack()
 
-        yes_button = tk.Button(pslit_window, text="Oui !", command=lambda: [report(output, "pslist.txt"), pslit_window.destroy()])
-        yes_button.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="e")
+        frame = tk.Frame(pslit_window)
+        frame.pack(pady=10)
 
-        no_button = tk.Button(pslit_window, text="Non !", command=pslit_window.destroy)
-        no_button.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+        yes_button = tk.Button(frame, text="Oui !", command=lambda: [report(output, "pslist.txt"), pslit_window.destroy()])
+        yes_button.pack(side="left", padx=20)
+
+        no_button = tk.Button(frame, text="Non !", command=pslit_window.destroy)
+        no_button.pack(side="left", padx=20)
 
         pslit_window.mainloop()
     
     else:
-
+       
         warning()
 
-def pstree():
 
+def pstree():
+    
     volatility_path = os.path.join(path(), "vol.py")
     config_json = os.path.join(path(), "config.json")
     memory_path = os.path.join(path(), "memory.mem")
@@ -272,32 +326,41 @@ def pstree():
         output, _ = pstree.communicate()
         output = output.decode("utf-8")
 
+        print(output)
+        
+        pause()
+        clean()
+
         pstree_file = "Souhaitez-vous enregistrer le résultat de l'analyse de l'arborescence des processus ?"
         tts(pstree_file)
 
         pstree_window = tk.Tk()
         pstree_window.title("Enregistrer le résultat de l'analyse de l'arborescence des processus :")
         pstree_window.iconbitmap(os.path.join(path(), "icon.ico"))
-        pstree_window.geometry("600x150")
+        pstree_window.geometry("600x120")
         pstree_window.resizable(False, False)
 
-        label = tk.Label(pstree_window, text=f"{pstree_file} :", padx=10, pady=10)
+        label = tk.Label(pstree_window, text=pstree_file, padx=10, pady=10)
         label.pack()
 
-        yes_button = tk.Button(pstree_window, text="Oui !", command=lambda: [report(output, "pstree.txt"), pstree_window.destroy()])
-        yes_button.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="e")
+        frame = tk.Frame(pstree_window)
+        frame.pack(pady=10)
 
-        no_button = tk.Button(pstree_window, text="Non !", command=pstree_window.destroy)
-        no_button.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+       
+        yes_button = tk.Button(frame, text="Oui !", command=lambda: [report(output, "pstree.txt"), pstree_window.destroy()])
+        yes_button.pack(side="left", padx=20)
+
+        no_button = tk.Button(frame, text="Non !", command=pstree_window.destroy)
+        no_button.pack(side="left", padx=20)
 
         pstree_window.mainloop()
     
     else:
-
+        
         warning()
 
 def dlllist():
-
+    
     volatility_path = os.path.join(path(), "vol.py")
     config_json = os.path.join(path(), "config.json")
     memory_path = os.path.join(path(), "memory.mem")
@@ -310,32 +373,41 @@ def dlllist():
         output, _ = dlllist.communicate()
         output = output.decode("utf-8")
 
+        print(output)
+        
+        pause()
+        clean()
+
         dlllist_file = "Souhaitez-vous enregistrer le résultat de l'ensemble des DLL qui ont été exécutées ?"
         tts(dlllist_file)
 
         dlllist_window = tk.Tk()
-        dlllist_window.title("Enregistrer le résultat de l'analyse des processus exécutés :")
+        dlllist_window.title("Enregistrer le résultat de l'ensemble des DLL exécutées :")
         dlllist_window.iconbitmap(os.path.join(path(), "icon.ico"))
-        dlllist_window.geometry("600x150")
+        dlllist_window.geometry("600x120")
         dlllist_window.resizable(False, False)
 
-        label = tk.Label(dlllist_window, text=f"{dlllist_file} :", padx=10, pady=10)
+        label = tk.Label(dlllist_window, text=dlllist_file, padx=10, pady=10)
         label.pack()
 
-        yes_button = tk.Button(dlllist_window, text="Oui !", command=lambda: [report(output, "dlllist.txt"), dlllist_window.destroy()])
-        yes_button.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="e")
+       
+        frame = tk.Frame(dlllist_window)
+        frame.pack(pady=10)
 
-        no_button = tk.Button(dlllist_window, text="Non !", command=dlllist_window.destroy)
-        no_button.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+        yes_button = tk.Button(frame, text="Oui !", command=lambda: [report(output, "dlllist.txt"), dlllist_window.destroy()])
+        yes_button.pack(side="left", padx=20)
+
+        no_button = tk.Button(frame, text="Non !", command=dlllist_window.destroy)
+        no_button.pack(side="left", padx=20)
 
         dlllist_window.mainloop()
-    
+  
     else:
-
+        
         warning()
 
 def netscan():
-
+    
     volatility_path = os.path.join(path(), "vol.py")
     config_json = os.path.join(path(), "config.json")
     memory_path = os.path.join(path(), "memory.mem")
@@ -348,32 +420,41 @@ def netscan():
         output, _ = netscan.communicate()
         output = output.decode("utf-8")
 
+        print(output)
+        
+        pause()
+        clean()
+
         netscan_file = "Souhaitez-vous enregistrer le résultat de l'analyse des connexions entrantes et sortantes ?"
         tts(netscan_file)
 
         netscan_window = tk.Tk()
         netscan_window.title("Enregistrer le résultat de l'analyse des connexions entrantes et sortantes :")
         netscan_window.iconbitmap(os.path.join(path(), "icon.ico"))
-        netscan_window.geometry("600x150")
+        netscan_window.geometry("600x120")
         netscan_window.resizable(False, False)
 
-        label = tk.Label(netscan_window, text=f"{netscan_file} :", padx=10, pady=10)
+        label = tk.Label(netscan_window, text=netscan_file, padx=10, pady=10)
         label.pack()
 
-        yes_button = tk.Button(netscan_window, text="Oui !", command=lambda: [report(output, "netscan.txt"), netscan_window.destroy()])
-        yes_button.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="e")
+        frame = tk.Frame(netscan_window)
+        frame.pack(pady=10)
 
-        no_button = tk.Button(netscan_window, text="Non !", command=netscan_window.destroy)
-        no_button.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+        yes_button = tk.Button(frame, text="Oui !", command=lambda: [report(output, "netscan.txt"), netscan_window.destroy()])
+        yes_button.pack(side="left", padx=20)
+
+        no_button = tk.Button(frame, text="Non !", command=netscan_window.destroy)
+        no_button.pack(side="left", padx=20)
 
         netscan_window.mainloop()
     
+   
     else:
-
+        
         warning()
 
 def cmdline():
-
+    
     volatility_path = os.path.join(path(), "vol.py")
     config_json = os.path.join(path(), "config.json")
     memory_path = os.path.join(path(), "memory.mem")
@@ -386,32 +467,40 @@ def cmdline():
         output, _ = cmdline.communicate()
         output = output.decode("utf-8")
 
+        print(output)
+        
+        pause()
+        clean()
+
         cmdline_file = "Souhaitez-vous enregistrer le résultat de l'analyse des processus ayant fait usage de commandes ?"
         tts(cmdline_file)
 
         cmdline_window = tk.Tk()
         cmdline_window.title("Enregistrer le résultat de l'analyse des processus ayant fait usage de commandes.")
         cmdline_window.iconbitmap(os.path.join(path(), "icon.ico"))
-        cmdline_window.geometry("600x150")
+        cmdline_window.geometry("600x120")
         cmdline_window.resizable(False, False)
 
-        label = tk.Label(cmdline_window, text=f"{cmdline_file} :", padx=10, pady=10)
+        label = tk.Label(cmdline_window, text=cmdline_file, padx=10, pady=10)
         label.pack()
 
-        yes_button = tk.Button(cmdline_window, text="Oui !", command=lambda: [report(output, "cmdline.txt"), cmdline_window.destroy()])
-        yes_button.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="e")
+        frame = tk.Frame(cmdline_window)
+        frame.pack(pady=10)
 
-        no_button = tk.Button(cmdline_window, text="Non !", command=cmdline_window.destroy)
-        no_button.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+        yes_button = tk.Button(frame, text="Oui !", command=lambda: [report(output, "cmdline.txt"), cmdline_window.destroy()])
+        yes_button.pack(side="left", padx=20)
+
+        no_button = tk.Button(frame, text="Non !", command=cmdline_window.destroy)
+        no_button.pack(side="left", padx=20)
 
         cmdline_window.mainloop()
     
     else:
-
+        
         warning()
 
 def key():
-
+   
     volatility_path = os.path.join(path(), "vol.py")
     config_json = os.path.join(path(), "config.json")
     memory_path = os.path.join(path(), "memory.mem")
@@ -424,28 +513,36 @@ def key():
         output, _ = key.communicate()
         output = output.decode("utf-8")
 
+        print(output)
+        
+        pause()
+        clean()
+
         key_file = "Souhaitez-vous enregistrer le résultat des clés de registre sous une ruche ou une valeur de clé spécifique ?"
         tts(key_file)
 
         key_window = tk.Tk()
         key_window.title("Enregistrer le résultat des clés de registre sous une ruche ou une valeur de clé spécifique.")
         key_window.iconbitmap(os.path.join(path(), "icon.ico"))
-        key_window.geometry("600x150")
+        key_window.geometry("600x120")
         key_window.resizable(False, False)
 
-        label = tk.Label(key_window, text=f"{key_file} :", padx=10, pady=10)
+        label = tk.Label(key_window, text=key_file, padx=10, pady=10)
         label.pack()
 
-        yes_button = tk.Button(key_window, text="Oui !", command=lambda: [report(output, "key.txt"), key_window.destroy()])
-        yes_button.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="e")
+        frame = tk.Frame(key_window)
+        frame.pack(pady=10)
 
-        no_button = tk.Button(key_window, text="Non !", command=key_window.destroy)
-        no_button.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+        yes_button = tk.Button(frame, text="Oui !", command=lambda: [report(output, "key.txt"), key_window.destroy()])
+        yes_button.pack(side="left", padx=20)
+
+        no_button = tk.Button(frame, text="Non !", command=key_window.destroy)
+        no_button.pack(side="left", padx=20)
 
         key_window.mainloop()
     
     else:
-
+        
         warning()
 
 def winsec():
@@ -453,15 +550,16 @@ def winsec():
     window = tk.Tk()
     window.title("Win'Sec :")
     window.iconbitmap(os.path.join(path(), "icon.ico"))
-    window.geometry("1000x600")
+    window.geometry("1250x800")
     window.resizable(False, False)
 
     menu = Menu(window)
     window.config(menu=menu)
 
     menu.add_cascade(label="Télécharger l'ensemble des logiciels.", command=download)
-    menu.add_cascade(label="Créer un fichier de configuration.", command=config)
+    menu.add_cascade(label="Supprimer l'ensemble des logiciels.", command=delete)
     menu.add_cascade(label="Créer une image mémoire.", command=memory)
+    menu.add_cascade(label="Créer un fichier de configuration.", command=config)
 
     analyze = Menu(menu, tearoff=0)
     menu.add_cascade(label="Analyser l'image mémoire.", menu=analyze)
@@ -486,4 +584,17 @@ def winsec():
 if __name__ == "__main__":
     
     ctypes.windll.kernel32.SetConsoleTitleW("Win'Sec :")
-    winsec()
+
+    if ctypes.windll.shell32.IsUserAnAdmin():
+        
+        clean()
+        winsec()
+
+    elif not ctypes.windll.shell32.IsUserAnAdmin():
+
+        admin = "Veuillez exécuter ce logiciel dans l'invite de commande en mode administrateur afin de l'utiliser de manière optimale !"
+
+        tts(admin)
+        messagebox.showwarning(title="Win'Sec :", message=admin)
+
+        quit()
